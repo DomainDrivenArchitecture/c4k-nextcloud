23619a7a61c5e23da8cd9eb6ea92d6fef6e21821
leo <leonard@witzleben.de>
2021-08-10 11:22:45 +0200

    fixed project.clj, compiles now

--- project.clj
+++ project.clj
@@ -1,55 +1,41 @@
 (defproject meissa/meissa-cloud "1.0.2-SNAPSHOT"
-  :description "Crate to install cloud"
-  :url "https://meissa-gmbh.de"
-  :license {:name "meissa commercial license"
-            :url "https://www.meissa-gmbh.de"}
-  :dependencies [[dda/dda-pallet "4.0.3"]
-                 [dda/dda-k8s-crate "1.0.1"]
-                 [orchestra "2021.01.01-1"]]
+  :description "nextcloud c4k-installation package"
+  :url "https://domaindrivenarchitecture.org"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.reader "1.3.4"]
+                 [org.domaindrivenarchitecture/c4k-common-clj "0.2.8"]]
   :target-path "target/%s/"
-  :source-paths ["main/src"]
-  :resource-paths ["main/resources"]
-  :repositories [["clojars" "https://clojars.org/repo"]
-                 ["snapshots"
-                  "https://artifact.prod.meissa-gmbh.de/repository/maven-snapshots/"]
-                 ["releases"
-                  "https://artifact.prod.meissa-gmbh.de/repository/maven-releases/"]]
-  :deploy-repositories [["snapshots" "https://artifact.prod.meissa-gmbh.de/repository/maven-snapshots/"]
-                        ["releases" "https://artifact.prod.meissa-gmbh.de/repository/maven-releases/"]]
-  :profiles {:dev {:source-paths ["integration/src"
-                                  "test/src"
-                                  "uberjar/src"]
-                   :resource-paths ["integration/resources"
-                                    "test/resources"]
-                   :dependencies
-                   [[org.clojure/test.check "1.1.0"]
-                    [dda/data-test "0.1.1"]
-                    [dda/pallet "0.9.1" :classifier "tests"]
-                    [ch.qos.logback/logback-classic "1.3.0-alpha5"]
-                    [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]
-                   :plugins
-                   [[lein-sub "0.3.0"]]
-                   :leiningen/reply
-                   {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
-                    :exclusions [commons-logging]}}
-             :test {:test-paths ["test/src"]
-                    :resource-paths ["test/resources"]
-                    :dependencies [[dda/pallet "0.9.1" :classifier "tests"]]}
-             :uberjar {:source-paths ["uberjar/src"]
-                       :resource-paths ["uberjar/resources"]
-                       :aot :all
+  :source-paths ["src/main/cljc"
+                 "src/main/clj"]
+  :resource-paths ["src/main/resources"]
+  :repositories [["snapshots" :clojars]
+                 ["releases" :clojars]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]]
+  :profiles {:test {:test-paths ["src/test/cljc"]
+                    :resource-paths ["src/test/resources"]
+                    :dependencies [[dda/data-test "0.1.1"]]}
+             :dev {:plugins [[lein-shell "0.5.0"]]}
+             :uberjar {:aot :all
                        :main dda.c4k-nextcloud.uberjar
                        :uberjar-name "c4k-nextcloud-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "1.0.194"]
-                                      [ch.qos.logback/logback-classic "1.3.0-alpha5"]
+                       :dependencies [[org.clojure/tools.cli "1.0.206"]
+                                      [ch.qos.logback/logback-classic "1.3.0-alpha4"
+                                       :exclusions [com.sun.mail/javax.mail]]
                                       [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]}}
-  :release-tasks [["vcs" "assert-committed"]
+  :release-tasks [["test"]
+                  ["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag"]
-                  ["deploy"]
-                  ["uberjar"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]]
-  :local-repo-classpath true)
+                  ["change" "version" "leiningen.release/bump-version"]]
+  :aliases {"native" ["shell"
+                      "native-image"
+                      "--report-unsupported-elements-at-runtime"
+                      "--initialize-at-build-time"
+                      "-jar" "target/uberjar/c4k-jira-standalone.jar"
+                      "-H:ResourceConfigurationFiles=graalvm-resource-config.json"
+                      "-H:Log=registerResource"
+                      "-H:Name=target/graalvm/${:name}"]})
