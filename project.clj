cce1fdc207d821200c49e7d95172de6e5d8b4b04
jem <michael.jerger@meissa-gmbh.de>
2021-09-14 19:55:58 +0200

    postgres now works

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.reader "1.3.4"]
-                 [org.domaindrivenarchitecture/c4k-common-clj "0.3.1"]]
+                 [org.domaindrivenarchitecture/c4k-common-clj "0.3.2-SNAPSHOT"]]
   :target-path "target/%s/"
   :source-paths ["src/main/cljc"
                  "src/main/clj"]
