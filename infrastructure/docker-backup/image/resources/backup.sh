cbd9bd3fd085e3c74620cc21ffb0d0818954eb19
ansgarz <ansgar.zwick@meissa-gmbh.de>
2021-12-08 11:27:17 +0100

    wip

--- infrastructure/docker-backup/image/resources/backup.sh
+++ infrastructure/docker-backup/image/resources/backup.sh
@@ -14,7 +14,7 @@ function main() {
     file_env RESTIC_DAYS_TO_KEEP 14
 
     backup-roles 'oc_'
-    ;backup-db-dump
+    #backup-db-dump
     echo test-content > /var/backups/tmpfile
     backup-directory '/var/backups/'
 
