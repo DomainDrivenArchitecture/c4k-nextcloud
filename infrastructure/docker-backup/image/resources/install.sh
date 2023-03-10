1757fcebb923dd69c4b42d445d09907bcaf14965
Michael Jerger <michael.jerger@meissa-gmbh.de>
2023-03-10 15:42:59 +0100

    introduce list-snapshot

--- infrastructure/docker-backup/image/resources/install.sh
+++ infrastructure/docker-backup/image/resources/install.sh
@@ -6,5 +6,6 @@ install -m 0700 /tmp/entrypoint-start-and-wait.sh /
 install -m 0700 /tmp/init.sh /usr/local/bin/
 install -m 0700 /tmp/backup.sh /usr/local/bin/
 install -m 0700 /tmp/restore.sh /usr/local/bin/
+install -m 0700 /tmp/list-snapshots.sh /usr/local/bin/
 install -m 0700 /tmp/start-maintenance.sh /usr/local/bin/
 install -m 0700 /tmp/end-maintenance.sh /usr/local/bin/
---
+++ infrastructure/docker-backup/image/resources/list-snapshots.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o pipefail
+
+function list-snapshot-files() {
+  if [ -z ${CERTIFICATE_FILE} ];
+  then
+    restic -r ${RESTIC_REPOSITORY}/${backup_file_path} snapshots
+  else
+    restic -r ${RESTIC_REPOSITORY}/${backup_file_path} snapshots --cacert ${CERTIFICATE_FILE}
+  fi
+}
+
+function main() {
+    file_env AWS_ACCESS_KEY_ID
+    file_env AWS_SECRET_ACCESS_KEY
+
+    file_env POSTGRES_DB
+    file_env POSTGRES_PASSWORD
+    file_env POSTGRES_USER
+
+    list-snapshot-roles
+    list-snapshot-db
+    list-snapshot-files
+}
+
+source /usr/local/lib/functions.sh
+source /usr/local/lib/file-functions.sh
+source /usr/local/lib/pg-functions.sh
+
+main
