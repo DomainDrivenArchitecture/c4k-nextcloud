2c0f3409033a3feac1c3cfde01a6c418c8deb25e
Michael Jerger <michael.jerger@meissa-gmbh.de>
2023-03-10 15:43:21 +0100

    enable restore parameter

--- infrastructure/docker-backup/image/resources/restore.sh
+++ infrastructure/docker-backup/image/resources/restore.sh
@@ -3,6 +3,10 @@
 set -Eeox pipefail
 
 function main() {
+    local role_snapshot_id="${1:-latest}"; shift
+    local db_snapshot_id="${1:-latest}"; shift
+    local file_snapshot_id="${1:-latest}"; shift
+
 
     start-maintenance.sh
 
@@ -15,17 +19,15 @@ function main() {
 
     drop-create-db
 
-    restore-roles
-    restore-db
-    restore-directory '/var/backups/'
+    restore-roles ${role_snapshot_id}
+    restore-db ${db_snapshot_id}
+    restore-directory '/var/backups/' ${file_snapshot_id}
 
     end-maintenance.sh
-
 }
 
 source /usr/local/lib/functions.sh
 source /usr/local/lib/pg-functions.sh
 source /usr/local/lib/file-functions.sh
 
-main
-
+main "$@"
