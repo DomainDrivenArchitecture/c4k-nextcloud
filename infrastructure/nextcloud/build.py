b51363bead3a62ed7e30d1e85a7454c531a0b8df
Mirco <mirco.zachmann@meissa.de>
2023-12-07 22:29:08 +0100

    release: 7.0.3

--- infrastructure/nextcloud/build.py
+++ infrastructure/nextcloud/build.py
@@ -6,7 +6,7 @@ from ddadevops import *
 name = 'c4k-cloud'
 MODULE = 'not_set'
 PROJECT_ROOT_PATH = '../..'
-version = "7.0.3-SNAPSHOT"
+version = "7.0.3"
 
 @init
 def initialize(project):
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-nextcloud "7.0.3-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-nextcloud "7.0.3"
   :description "nextcloud c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
