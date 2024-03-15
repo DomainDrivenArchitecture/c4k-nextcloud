7d9ca203bb2545e6da6e556285a09e3a7216bc92
bom <mattis.boeckle@meissa-gmbh.de>
2024-03-15 15:36:34 +0100

    bump version to: 8.0.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-nextcloud "8.0.4"
+(defproject org.domaindrivenarchitecture/c4k-nextcloud "8.0.5-SNAPSHOT"
   :description "nextcloud c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
