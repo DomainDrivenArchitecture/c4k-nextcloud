f108b67e62c435767e8bec2309ff6b0f95639494
Mirco <mirco.zachmann@meissa.de>
2024-04-12 11:50:21 +0200

    lint fix use current versions

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.11.2"]
                  [org.clojure/tools.reader "1.4.1"]
-                 [org.domaindrivenarchitecture/c4k-common-clj "6.2.2"]
+                 [org.domaindrivenarchitecture/c4k-common-clj "6.2.3"]
                  [hickory "0.7.1" :exclusions [viebel/codox-klipse-theme]]]
   :target-path "target/%s/"
   :source-paths ["src/main/cljc"
@@ -23,7 +23,7 @@
                        :main dda.c4k-nextcloud.uberjar
                        :uberjar-name "c4k-nextcloud-standalone.jar"
                        :dependencies [[org.clojure/tools.cli "1.1.230"]
-                                      [ch.qos.logback/logback-classic "1.5.3"
+                                      [ch.qos.logback/logback-classic "1.5.4"
                                        :exclusions [com.sun.mail/javax.mail]]
                                       [org.slf4j/jcl-over-slf4j "2.0.12"]
                                       [com.github.clj-easy/graal-build-time "1.0.5"]]}}
