8388d7251736f7c3a63e885380527d98b6b95b59
Michael Jerger <michael.jerger@meissa-gmbh.de>
2024-05-31 17:03:56 +0200

    release: 10.2.0

--- infrastructure/backup/build.py
+++ infrastructure/backup/build.py
@@ -6,7 +6,7 @@ from ddadevops import *
 name = "c4k-cloud"
 MODULE = "backup"
 PROJECT_ROOT_PATH = "../.."
-version = "10.1.1-dev"
+version = "10.2.0"
 
 
 @init
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-nextcloud "10.1.1-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-nextcloud "10.2.0"
   :description "nextcloud c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
