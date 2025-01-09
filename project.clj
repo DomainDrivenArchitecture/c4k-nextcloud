304bb3c967d801c4ea0477e607a5a200209e5513
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-01-09 14:03:43 +0100

    update dep

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/tools.reader "1.5.0"]
-                 [org.domaindrivenarchitecture/c4k-common-clj "8.1.1"]
+                 [org.domaindrivenarchitecture/c4k-common-clj "9.0.1"]
                  [hickory "0.7.1" :exclusions [viebel/codox-klipse-theme]]]
   :target-path "target/%s/"
   :source-paths ["src/main/cljc"
