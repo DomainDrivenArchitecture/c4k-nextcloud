020d9559e1429ac7483be0b481f23c6e417b8069
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-01-13 13:59:09 +0100

    bump version to: 10.5.1-SNAPSHOT

--- infrastructure/nextcloud/build.py
+++ infrastructure/nextcloud/build.py
@@ -6,7 +6,7 @@ from ddadevops import *
 name = 'c4k-cloud'
 MODULE = 'not_set'
 PROJECT_ROOT_PATH = '../..'
-version = "10.5.0"
+version = "10.5.1-dev"
 
 @init
 def initialize(project):
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-nextcloud "10.5.0"
+(defproject org.domaindrivenarchitecture/c4k-nextcloud "10.5.1-SNAPSHOT"
   :description "nextcloud c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
